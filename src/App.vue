eaf93603920c1688154621149ccade6ec0b20f15
Ten <782555894@qq.com>
2020-05-09 09:49:29 +0800

    one dayy

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    App根组件
+      <!--路由占位符-->
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'app'
 }
 </script>
 
